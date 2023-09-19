b61e4d48c21aafc2ac6e1c692bd4dcf6abd444f3
Or <or.tzur@mail.huji.ac.il>
2023-09-19 14:05:47 +0300

    No commit

---
+++ oop/Bucket.java
@@ -0,0 +1,35 @@
+package oop;
+
+public class Bucket {
+    private int capacity;
+    private double AmountFilled = 0;
+
+    public Bucket(int capacity){
+        this.capacity = capacity;
+    }
+    public void empty(){
+        this.AmountFilled = 0;
+    }
+    public boolean isEmpty(){
+        return this.AmountFilled ==0;
+    }
+    public void fill(double AmountToFill){
+        this.AmountFilled += AmountToFill;
+        if( this.AmountFilled > this.capacity){
+            this.AmountFilled = this.capacity;
+        }
+    }
+    public int getCapacity(){
+        return this.capacity;
+    }
+    public double getCurrentAmount(){
+        return this.AmountFilled;
+    }
+    public void pourInto(Bucket bucketInto){
+        bucketInto.fill(AmountFilled);
+        this.empty();
+    }
+    public String toString(){
+        return "The capacity: " + this.capacity + "\nThe current amount of water: " + this.AmountFilled;
+    }
+}
--- oop/Date.java
+++ oop/Date.java
@@ -9,6 +9,33 @@ public class Date {
         this.month = month;
         this.year = year;
     }
-    public int getYear(int day, )
+    public int getYear(){
+        return this.year;
+    }
+    public int getMonth(){
+        return this.month;
+    }
+    public int getDay(){
+        return this.day;
+    }
+    public void setYear(int yearToSet){
+        this.year = yearToSet;
+    }
+    public void setMonth(int monthToSet){
+        this.month = monthToSet;
+    }
+    public void setDay(int dayToSet){
+        this.day = dayToSet;
+    }
+    public int compareTo(Date dateToCompare){
+        int DiffrentOfyear = this.year - dateToCompare.year;
+        int DiffrentOfmonth = this.month - dateToCompare.month;
+        int DiffrentOfday = this.day - dateToCompare.day;
+        int DiffrentOfdate = DiffrentOfyear*12*31 + DiffrentOfmonth*31 + DiffrentOfday;
+        return DiffrentOfdate;
+    }
+    public String toString(){
+        return this.day + "." + this.month + "." + this.year + ".";
+    }
     
 }
