377ef7a3389f9673b764eb3bbed78b7e4e811d66
Or <or.tzur@mail.huji.ac.il>
2023-09-19 14:06:24 +0300

    NoCommit

---
+++ oop/TestOPP.java
@@ -0,0 +1,11 @@
+package oop;
+
+public class TestOPP {
+    public static void main(String[] args) {
+        Bucket onw = new Bucket(20);
+        System.out.println(onw.isEmpty());
+        onw.empty();
+        System.out.println(onw.isEmpty());
+        Bucket two = new Bucket(0);
+    }
+}
