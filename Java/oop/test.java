0ca9763519e1f0a095167e61cd7acc1d59d7e662
Or-lp-ubuntu <or.tzur@mail.huji.ac.il>
2023-09-20 23:27:31 +0300

    No commit

--- Java/oop/test.java
+++ Java/oop/test.java
@@ -1,8 +1,15 @@
 package oop;
-
+import java.util.Vector;
+//import java.util.Point
 public class test {
+
     public static void main(String[] args) {
-        int a=0;
+        Vector v = new Vector();
+        v.add(new Point(1, 2));
+        v.add(new Point(3, 4));
+        v.add(new Point(5, 6));
+        v.add(2);
+        System.out.println(v.get(0));
         
         
     }
